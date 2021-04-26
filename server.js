a4947e269663d4834ece62ef0efb753bb5aa5d21
Judecx14 <19030119@uttcampus.edu.mx>
2021-04-26 12:06:17 -0500

    Experimentooi

--- server.js
+++ server.js
@@ -5,11 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/smart-garden'));
-
-app.get('/*', (req, res) =>
-    res.sendFile({root: 'dist/smart-garden/index.html'}),
-);
-
+app.use(express.static(path.join(__dirname, 'dist','smart-garden')));
+res.sendFile(path.join((__dirname,'dist','smart-garden','index.html')));
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
