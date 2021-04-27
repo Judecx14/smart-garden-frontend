df3bf752887a13d10511c1838b047d282190a594
Judecx14 <19030119@uttcampus.edu.mx>
2021-04-26 19:27:04 -0500

    Creo

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('dist/smart-garden'));
+app.use(express.static('./dist/SmartGarden'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/smart-garden/'}),
+    res.sendFile('index.html', {root: 'dist/SmartGarden/'}),
 );
 
 // Start the app by listening on the default Heroku port
